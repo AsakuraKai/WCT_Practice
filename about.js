d2361fd83a42fe63e0151128009b5d8d20688a43
Bun Sovann <bunsovan674@gmail.com>
2024-12-28 15:05:28 +0700

    about.js

---
+++ about.js
@@ -0,0 +1,4 @@
+document.getElementById("alertButton").addEventListener("click", function () {
+    alert("Button clicked!");
+    document.getElementById("message").textContent = "You clicked the button!";
+});
